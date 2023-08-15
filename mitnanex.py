cb1de863f74a748b5aa8f127259a34dc6696c749
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-08-15 11:41:08 -0700

    start coding

---
+++ mitnanex.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#a = pd.read_csv('overlap_region_0_1000_nuclear.paf',delimiter='\t', header=None)
+b = {"a":np.array(["3"*1000])}
+print(hash(b))
