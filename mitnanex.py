86441b5110f2081fb0f79b93c79acbbae047e3e1
Juan Jose Picon Cossio <jpiconco@uoregon.edu>
2023-08-15 12:07:55 -0700

    updating gitignore

--- mitnanex.py
+++ mitnanex.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-#a = pd.read_csv('overlap_region_0_1000_nuclear.paf',delimiter='\t', header=None)
-b = {"a":np.array(["3"*1000])}
-print(hash(b))
-#hola
+chunksize = 1000
+for chunk in pd.read_csv('', chunksize=chunksize):
+    print (chunk.head())
+    break
