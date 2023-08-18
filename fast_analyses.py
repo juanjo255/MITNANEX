2014cb0fc98a3bb4003ff65e6172bfcd1457e377
Juan Jose Picon Cossio <jpiconco@uoregon.edu>
2023-08-18 15:40:43 -0700

    fast_analyses to do some plot and see if everything is working correctly

--- fast_analyses.py
+++ fast_analyses.py
@@ -1,11 +1,5 @@
 import numpy as np
-
-# file = pd.read_csv('overlaps_talaro_18_07_2023.paf',delimiter="\t", header=None)
-# file['coverage'] = ((file[3]-file[2])/file[1])
-# file['len'] = file[3]-file[2]
 import time
+from mitnanex import run
 
-a = "      "
-while a.strip():
-    print(a)
-    break
+a = run()
