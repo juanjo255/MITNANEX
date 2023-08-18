a51e88c7efb4d75d0aafe7b626c3c8daa596e949
Juan Jose Picon Cossio <jpiconco@uoregon.edu>
2023-08-18 14:28:29 -0700

    first test

--- fast_analyses.py
+++ fast_analyses.py
@@ -4,24 +4,8 @@ import numpy as np
 # file['coverage'] = ((file[3]-file[2])/file[1])
 # file['len'] = file[3]-file[2]
 import time
-def prueba ():
-    
-    a = []
-    for i in range(1, 100000):
-        a.append(i)
-    inicio = time.time()
-    [i for i in a if i%2==0]
-    final = time.time()
-    total = final-inicio
-    print("lista",total)
 
-    b = np.array([])
-    for i in range(1, 100000):
-        b = np.append(b,i)
-    inicio = time.time()
-    np.where(b%2==0)
-    final = time.time()
-    total = final-inicio
-    print("array conditions",total)
-
-prueba()
+a = "      "
+while a.strip():
+    print(a)
+    break
