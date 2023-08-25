b867f73e427b75a208ed5306831946bd1f288f98
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-08-25 13:32:25 -0700

    checkpoint: se ha logrado separar los reads con mayor cobertura y menor porcentage de GC

--- fast_analyses.py
+++ fast_analyses.py
@@ -3,18 +3,44 @@ from src.mitnanex import run
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from sklearn.cluster import KMeans
 
 inicio = time.time()
 a = run()
 
 # I need to plot the coverage of clusters 
 coverages_df = pd.DataFrame({"coverage":[i.coverage for i in a.clusters], "longest_read_len":[i.longest_read_length for i in a.clusters], "id_longest_read": [i.longest_read_id for i in a.clusters] })
-coverages_df.sort_values(by='coverage',inplace=True, ascending=False)
-#print(coverages_df.duplicated('id_longest_read').sum())
+#coverages_df.sort_values(by='coverage',inplace=True, ascending=False)
+coverages_df ['GC_percentage'] = 0
+file = open('../all_talaro_porechop_18_07_2023.fastq', 'r')
+oneline = file.readline()
+while oneline:
+    if oneline.startswith("@") and len(oneline.split(" ")) > 1:
+        id = (oneline.split(" ")[0].strip())[1:]
+        if id in coverages_df['id_longest_read'].values:
+            # get ADN (it's the line following the id line)
+            #print(id)
+            sequence=file.readline()
+            cg_count = sequence.count('C') + sequence.count("G")
+            coverages_df.loc[coverages_df["id_longest_read"]==id,'GC_percentage'] = cg_count/len(sequence)
+            
+    oneline=file.readline()
+
+print(coverages_df.head())
 final = time.time()
 tiempo = final - inicio
 print("my code", tiempo)
 
+## First view before Kmeans
+#plt.scatter(x=coverages_df['GC_percentage'], y=coverages_df['coverage'])
+
+kmeans = KMeans(n_clusters=2, max_iter=100, init='k-means++', random_state=0, n_init=1, verbose=1)
+prediction = kmeans.fit_predict(coverages_df.loc[:, ['coverage','GC_percentage']])
+
+
+plt.scatter(x=coverages_df['GC_percentage'], y=coverages_df['coverage'], c=prediction)
+
+plt.show()
 # gc_content = pd.read_csv('longest_read_every_cluster_gc_content.tsv', delimiter='\t',header=None)
 # df = gc_content[gc_content[2] < 30]
 # print(df.describe())
