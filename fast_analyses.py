27dc0f2801e1f5a615acb035e42b51c84ce06171
Juan Jose Picon Cossio <jpiconco@uoregon.edu>
2023-08-28 12:45:48 -0700

    adding __init__.py for imports

--- fast_analyses.py
+++ fast_analyses.py
@@ -7,23 +7,31 @@ from sklearn.cluster import KMeans
 inicio = time.time()
 a = run()
 
-# I need to plot the coverage of clusters 
-coverages_df = pd.DataFrame({"coverage":[i.coverage for i in a.clusters], "longest_read_len":[i.longest_read_length for i in a.clusters], "id_longest_read": [i.longest_read_id for i in a.clusters] })
-#coverages_df.sort_values(by='coverage',inplace=True, ascending=False)
-coverages_df ['GC_percentage'] = 0
-file = open('test/aedes_vexans_mt_reads_subsample.fastq', 'r')
+# I need to plot the coverage of clusters
+coverages_df = pd.DataFrame(
+    {
+        "coverage": [i.coverage for i in a.clusters],
+        "longest_read_len": [i.longest_read_length for i in a.clusters],
+        "id_longest_read": [i.longest_read_id for i in a.clusters],
+    }
+)
+# coverages_df.sort_values(by='coverage',inplace=True, ascending=False)
+coverages_df["GC_percentage"] = 0
+file = open("test/aedes_vexans_mt_reads_subsample.fastq", "r")
 oneline = file.readline()
 while oneline:
     if oneline.startswith("@") and len(oneline.split(" ")) > 1:
         id = (oneline.split(" ")[0].strip())[1:]
-        if id in coverages_df['id_longest_read'].values:
+        if id in coverages_df["id_longest_read"].values:
             # get ADN (it's the line following the id line)
-            #print(id)
-            sequence=file.readline()
-            cg_count = sequence.count('C') + sequence.count("G")
-            coverages_df.loc[coverages_df["id_longest_read"]==id,'GC_percentage'] = cg_count/len(sequence)
-            
-    oneline=file.readline()
+            # print(id)
+            sequence = file.readline()
+            cg_count = sequence.count("C") + sequence.count("G")
+            coverages_df.loc[
+                coverages_df["id_longest_read"] == id, "GC_percentage"
+            ] = cg_count / len(sequence)
+
+    oneline = file.readline()
 
 print(coverages_df.describe())
 final = time.time()
@@ -31,20 +39,22 @@ tiempo = final - inicio
 print("my code", tiempo)
 
 ## First view before Kmeans
-#plt.scatter(x=coverages_df['GC_percentage'], y=coverages_df['coverage'])
+# plt.scatter(x=coverages_df['GC_percentage'], y=coverages_df['coverage'])
 
-kmeans = KMeans(n_clusters=3, max_iter=100, init='k-means++', random_state=0, n_init=1, verbose=1)
-prediction = kmeans.fit_predict(coverages_df.loc[:, ['coverage','GC_percentage']])
+kmeans = KMeans(
+    n_clusters=3, max_iter=100, init="k-means++", random_state=0, n_init=1, verbose=1
+)
+prediction = kmeans.fit_predict(coverages_df.loc[:, ["coverage", "GC_percentage"]])
 
 
-plt.scatter(x=coverages_df['GC_percentage'], y=coverages_df['coverage'], c=prediction)
+plt.scatter(x=coverages_df["GC_percentage"], y=coverages_df["coverage"], c=prediction)
 
 plt.show()
 # gc_content = pd.read_csv('longest_read_every_cluster_gc_content.tsv', delimiter='\t',header=None)
 # df = gc_content[gc_content[2] < 30]
 # print(df.describe())
-#gc_content.hist(bins=2)
-#plt.show()
+# gc_content.hist(bins=2)
+# plt.show()
 
 # inicio = time.time()
 # file = pd.read_csv("overlaps_talaro_18_07_2023.paf",delimiter='\t',header=None)
--- src/cluster.py
+++ src/cluster.py
@@ -1,4 +1,4 @@
-from src.psa import psa
+from .psa import psa
 class cluster:
     """
     Store each cluster of reads, each cluster contains reads in the form of their id
