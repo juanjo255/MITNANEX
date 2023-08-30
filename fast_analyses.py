f05aeb9bc1b38e3cdf4cf0fba04ab6694bf39f81
Juan Jose Picon Cossio <jpiconco@uoregon.edu>
2023-08-30 16:26:39 -0700

    checkpoint

--- fast_analyses.py
+++ fast_analyses.py
@@ -4,48 +4,40 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.cluster import KMeans
 from Bio import SeqIO
-from src.utils import convert_fq_to_fa
+from sourmash import MinHash
 
-## FASTA file is lighter 
-convert_fq_to_fa('test/aedes_vexans_all_reads_subsample_sorted_length.fastq', 'test/aedes_vexans_all_reads_subsample_sorted_length.fasta')
-
-# inicio = time.time()
-# a = run()
-
-# # I need to plot the coverage of clusters
-# coverages_df = pd.DataFrame(
-#     {
-#         "coverage": [i.coverage for i in a.clusters],
-#         "longest_read_len": [i.longest_read_length for i in a.clusters],
-#         "id_longest_read": [i.longest_read_id for i in a.clusters],
-#         "id_cluster":[i.id_cluster for i in a.clusters],
-#     }
+## FASTA file is lighter
+# convert_fq_to_fa(
+#     "test/aedes_vexans_all_reads_subsample_sorted_length.fastq",
+#     "test/aedes_vexans_all_reads_subsample_sorted_length.fasta",
 # )
-# # coverages_df.sort_values(by='coverage',inplace=True, ascending=False)
-# coverages_df["GC_percentage"] = 0
-# file = open("test/aedes_vexans_mt_reads_subsample.fastq", "r")
-# oneline = file.readline()
-# while oneline:
-#     if oneline.startswith("@") and len(oneline.split(" ")) > 1:
-#         id = (oneline.split(" ")[0].strip())[1:]
-#         if id in coverages_df["id_longest_read"].values:
-#             # get ADN (it's the line following the id line)
-#             # print(id)
-#             sequence = file.readline()
-#             cg_count = sequence.count("C") + sequence.count("G")
-#             coverages_df.loc[
-#                 coverages_df["id_longest_read"] == id, "GC_percentage"
-#             ] = cg_count / len(sequence)
+records_dict = SeqIO.to_dict(
+        SeqIO.parse("test/aedes_vexans_all_reads_subsample_sorted_length.fasta", "fasta")
+    )
 
-#     oneline = file.readline()
 
-# #print(coverages_df.describe())
-# prueba = coverages_df.sort_values(by='coverage', ascending=False).head(50)
-# final = time.time()
-# tiempo = final - inicio
-# print("my code", tiempo)
+inicio = time.time()
+a = run()
 
-#print("323a391b-0c08-46bd-bc32-26ecbadfed9f" in a.clusters[10].id_sequences)
+# I need to plot the coverage of clusters
+coverages_df = pd.DataFrame(
+    {
+        "coverage": [i.coverage for i in a.clusters],
+        "longest_read_len": [i.longest_read_length for i in a.clusters],
+        "id_longest_read": [i.longest_read_id for i in a.clusters],
+        "id_cluster":[i.id_cluster for i in a.clusters],
+    }
+)
+
+# Test on the first 30 clusters
+prueba = coverages_df.sort_values(by='coverage', ascending=False).head(30)
+for id in prueba['id_longest_read']:
+    records_dict[id].seq
+
+final = time.time()
+tiempo = final - inicio
+print("my code", tiempo)
+
 
 ## First view before Kmeans
 # plt.scatter(x=coverages_df['GC_percentage'], y=coverages_df['coverage'])
@@ -66,7 +58,6 @@ convert_fq_to_fa('test/aedes_vexans_all_reads_subsample_sorted_length.fastq', 't
 # print(file.describe())
 # file = file[file[10]>500]
 # print(file.describe())
-#file_grouped = (file.groupby(by=0))
-#print(file_grouped.head())
-#print(file.sort_values(by=1, ascending=False).head())
-
+# file_grouped = (file.groupby(by=0))
+# print(file_grouped.head())
+# print(file.sort_values(by=1, ascending=False).head())
