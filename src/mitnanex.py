3292eebd80988b84b738e586e61bbd72cac601b3
Juan Jose Picon Cossio <jpiconco@uoregon.edu>
2023-09-04 15:54:44 -0700

    adding kmer counting

--- src/mitnanex.py
+++ src/mitnanex.py
@@ -1,6 +1,7 @@
 from .hash_function import hash_table_ids, hash_table_clusters, estimate_hash_table_size
 from .psa import psa
 from .assign_cluster import assign_cluster
+from .utils import get_sequences_by_id
 
 
 ## PAF FORMAT
@@ -25,13 +26,13 @@ def run() -> hash_table_clusters:
     file = open("test/overlaps_aedes_vexans_all_reads_subsample_sorted_containment.paf", "r")
     #size_table = estimate_hash_table_size('test/aedes_vexans_all_reads_subsample_sorted_length.fastq')
     alignment = file.readline().strip()
-    clusters = hash_table_clusters()
+    clusters_list = hash_table_clusters()
     cluster_pointers = hash_table_ids(size_table=int(1e5))
 
     # Iterate through each alignment
     while alignment:
         alignment = psa(alignment.strip().split("\t"))
-        assign_cluster(alignment, cluster_pointers, clusters)
+        assign_cluster(alignment, cluster_pointers, clusters_list)
 
         # New alignment
         alignment = file.readline().strip()
@@ -44,7 +45,13 @@ def run() -> hash_table_clusters:
 
     # duplicated_clusters = unique_longest_reads[count_unique_longest_reads > 1]
     # print(duplicated_clusters)
-    return clusters
+
+    ## Get cluster sequence and cluster
+    # repr_reads = [cluster.longest_read_id for cluster in clusters_list.clusters]
+    # get_sequences_by_id(repr_reads)
+    
+    
+    return clusters_list
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,15 @@
 from Bio import SeqIO
 
-def convert_fq_to_fa (file:str, output:str) -> None:
+def convert_fq_to_fa (fastq:str, output:str) -> None:
     
-    with open(file) as handle:
+    with open(fastq) as handle:
         count_sequences = SeqIO.convert(handle, "fastq", output, 'fasta')
         print(f"{count_sequences} sequences converted")
+
+
+def get_sequences_by_id(fasta:str, ids:list):
+    with open(fasta) as handle:
+        for record in SeqIO.parse(handle, "fasta"):
+            if record.id in ids:
+                #print(record.id)                
+                yield (record.id, record.seq)
