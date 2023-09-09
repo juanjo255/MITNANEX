6feb15cf39e3a95df1b5a38695da9042e0ea310c
Juan Jose Picon Cossio <jpiconco@uoregon.edu>
2023-09-09 10:52:16 -0700

    checkpoint

--- src/utils.py
+++ src/utils.py
@@ -11,5 +11,4 @@ def get_sequences_by_id(fasta:str, ids:list):
     with open(fasta) as handle:
         for record in SeqIO.parse(handle, "fasta"):
             if record.id in ids:
-                #print(record.id)                
                 yield (record.id, record.seq)
