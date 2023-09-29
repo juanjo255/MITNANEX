9584604d1ef6e0166f5948800267adb96965a1f9
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-09-29 10:44:48 -0700

    checkpoint

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,7 @@ def convert_fq_to_fa (fastq:str, output:str) -> str:
 
 def get_sequences_by_id(fasta:str, ids:list) -> tuple:
     with open(fasta) as handle:
-        for record in SeqIO.parse(handle, "fasta"):
+        for record in SeqIO.parse(handle, "fastq"):
             if record.id in ids:
                 yield (record.id, record.seq)
     return
@@ -17,7 +17,7 @@ def write_fasta(fasta:str, sequences_ids:set, output:str):
     final_records = list()
     n=0
     with open(fasta) as handle:
-        for record in SeqIO.parse(handle, "fasta"):
+        for record in SeqIO.parse(handle, "fastq"):
             if (record.id).strip() in sequences_ids:
                 n+=1
                 final_records.append(record)
