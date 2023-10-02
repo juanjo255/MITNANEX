9cfcf4f1e46a3bbab5e5daa71b9aceb033a60be4
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-10-01 19:53:41 -0700

    checkpoint

--- mitnanex_rs/src/kmer_cnt.rs
+++ mitnanex_rs/src/kmer_cnt.rs
@@ -1,11 +1,34 @@
+use std::cmp;
+
 use itertools::Itertools;
 
-pub fn possible_kmers (k:u8, ncl:&str){
-    let total_kmers = (1..=k).cartesian_product(ncl.chars());
-    println!("{:?}",total_kmers);
+pub fn reverse_compl(seq:&str) -> String {
+    let mut rev_comp = String::new();
+    for letter in seq.chars(){
+        match letter{
+            'A' => rev_comp= String::from("T") + &rev_comp,
+            'T' => rev_comp= String::from("A") + &rev_comp,
+            'C' => rev_comp= String::from("G") + &rev_comp,
+            'G' => rev_comp= String::from("C") + &rev_comp,
+            _ => panic!("There is some non-canonical nucleotides!")
+        };
+    };
+    return rev_comp
 }
 
-pub fn count_kmer (k:u8,seq:&str) {
+pub fn possible_kmers(k: usize, ncl: &str) -> Vec<String> {
+    // Create all kmers possible for a value K and keep only canonical kmers
+
+    let total_kmers = (1..=k).map(|_| ncl.chars()).multi_cartesian_product().map(|x| x.iter().join("")).collect_vec();
+    let canon_kmers = total_kmers.into_iter().filter(
+        |x| {
+            &x == cmp::min(&x, &&reverse_compl(&x))
+            }
+        ).collect_vec();
+    return canon_kmers;
+}
+
+pub fn count_kmer(k: u8, seq: &str) {
     // Count kmers in a dna sequence using hash table
     todo!()
 }
