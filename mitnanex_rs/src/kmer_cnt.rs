0bd31258043251fb8264f5b4fc66729c59f9127f
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-10-02 18:05:49 -0700

    checkpoint: reverse complement function from bio rust added

--- mitnanex_rs/src/kmer_cnt.rs
+++ mitnanex_rs/src/kmer_cnt.rs
@@ -1,5 +1,6 @@
 use itertools::Itertools;
-use std::{cmp, usize, collections::HashMap};
+use std::{cmp, usize, collections::HashMap, str};
+use bio::alphabets::dna;
 
 pub fn reverse_compl(seq: &str) -> String {
     let mut rev_comp = String::new();
@@ -30,16 +31,17 @@ pub fn possible_kmers(k: usize, ncl: &str) -> Vec<String> {
     return canon_kmers;
 }
 
-pub fn count_kmer(k: usize, seq: &str) -> HashMap<String, f64> {
+pub fn count_kmer(k: usize, seq: &str) -> HashMap<String, f32> {
     // Count kmers in a dna sequence using hash table
     let len_seq:usize = seq.len();
     let num_kmer_expected:usize = len_seq - k + 1;
     let possible_kmers_list:Vec<String> = possible_kmers(k, "ATCG");
-    let mut kmer_counter:HashMap<String, f64> = HashMap::from_iter(possible_kmers_list.into_iter().map(|x| (x, 1.0)));
+    let mut kmer_counter:HashMap<String, f32> = HashMap::from_iter(possible_kmers_list.into_iter().map(|x| (x, 1.0)));
 
     for kmer in 0..num_kmer_expected {
         let kmer_for = &seq[kmer..kmer+k];
-        let kmer_rev = reverse_compl(kmer_for);
+        let kmer_rev = dna::revcomp(kmer_for.as_bytes());
+        let kmer_rev = str::from_utf8(&kmer_rev).unwrap();
         let canon_kmer = cmp::min(kmer_for, &kmer_rev);
         if let Some(x) = kmer_counter.get_mut(canon_kmer){
             *x+= 1.0;
@@ -51,13 +53,13 @@ pub fn count_kmer(k: usize, seq: &str) -> HashMap<String, f64> {
     // Normalize kmer count by read length
     for kmer in kmer_counter.clone().keys(){
         if let Some(x) = kmer_counter.get_mut(kmer){
-            *x = *x / len_seq as f64;
+            *x = *x / len_seq as f32;
         };
     };
     return kmer_counter
 }
 
-pub fn get_kmer_profiles (reads:&Vec<&str>, reads_file:&str) -> Vec<f64>{
+pub fn get_kmer_profiles (reads:&Vec<&str>, reads_file:&str) -> Vec<f32>{
     
     todo!()
 }
