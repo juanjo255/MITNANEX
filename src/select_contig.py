0dbd3cc9fe920d75f80d543251253dd27e83e4b3
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-10-07 12:22:24 -0700

    checkpoint: adding function to select a contig from flye results
    Former-commit-id: 487e29dc04a822ccb2867cc4f49d47ce9dbea714

--- src/select_contig.py
+++ src/select_contig.py
@@ -1,19 +1,50 @@
+import pandas as pd
 from Bio import SeqIO
 
-def select_contig (flye_metadata:str, fasta:str):
-    """ Sometimes we can get more than one contig because of the 
 
-    Args:
-        fasta (str): _description_
-        size_overlap (int, optional): _description_. Defaults to 1000.
-        ksize (int, optional): _description_. Defaults to 3.
+def select_contig(flye_metadata: str, fasta: str = None) -> str:
+    """Select the contig to use to use in minimap2 continue summoning reads for a better assembly.
 
     Returns:
-        _type_: _description_
+        str: Sequence of the selected contig.
     """
-    with open(flye_metadata) as flye_metadata_handle:
-        with open(fasta) as fasta_handle:
-            for record in SeqIO.parse(fasta_handle, "fasta"):
-                pass
+    flye_metadata_df = pd.read_csv(flye_metadata, delimiter="\t", header=0)
+    flye_metadata_df.sort_values(by=["length", "cov."], inplace=True)
 
-            return 
+    ## Check if there is a circular genome. If so and there are more than one, select the longest
+    if flye_metadata_df.empty:
+        print("Not assembly found!!")
+        raise (ValueError("Not assembly found!"))
+    elif not (flye_metadata_df[flye_metadata_df["circ."] == "Y"].empty):
+        print("A circular genome found. Selecting... ")
+        flye_metadata_df = flye_metadata_df[
+            flye_metadata_df["circ."] == "Y"
+        ].sort_values(by="length")
+        return get_contig_sequence(flye_metadata_df["#seq_name"][0], fasta)
+    else:
+        return get_contig_sequence(flye_metadata_df["#seq_name"][0], fasta)
+
+
+select_contig("/Users/jjpc/flye_s_cervisae_CEN_PK113-7D/assembly_info_2.txt")
+
+
+def get_contig_sequence(contig: str, fasta: str) -> str:
+    """look for the sequence of the selected contig
+
+    Args:
+        contig (str): selected contig.
+        fasta (str): file returned by flye.
+
+    Returns:
+        str: Selected contig sequence.
+    """
+    with open(fasta) as fasta_handle:
+        for record in SeqIO.parse(fasta_handle, "fasta"):
+            if record.id == contig:
+                return record.seq
+            else:
+                raise (
+                    ValueError(
+                        "Something weird happend. I did not find the sequence of the contig."
+                    )
+                )
