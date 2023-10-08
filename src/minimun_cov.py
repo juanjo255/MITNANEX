518561d45eff1f38efd22f44d61691b244f1c4a3
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-10-08 11:46:26 -0700

    checkpoint: preparing bash script to run all
    Former-commit-id: a3ca02094e51610f08b36d7c47c0a0b0ff3ed83e

--- src/minimun_cov.py
+++ src/minimun_cov.py
@@ -1,21 +1,24 @@
 from sklearn.neighbors import KernelDensity
 from scipy.signal import argrelextrema
+import numpy as np
+import pandas as pd
 
-def set_minimun_cov (clusters_info, args):
+
+def set_minimun_cov(clusters_info: pd.DataFrame, coverage: str) -> int:
     ## Get minimum coverage
     kde = KernelDensity(kernel="gaussian", bandwidth=0.5).fit(
         clusters_info["coverage"].array.reshape(-1, 1)
     )
     cov_fdp = kde.score_samples(clusters_info["coverage"].array.reshape(-1, 1))
     local_min = argrelextrema(cov_fdp, np.less)[0]
-    
+
     ## Check if the user set a coverage
-    if int(args[3]) > -1:
+    if int(coverage) > -1:
         if len(local_min) < 1:
             min_coverage = 2
         else:
             min_coverage = clusters_info.iloc[max(local_min), :]["coverage"]
     else:
-        min_coverage = args[3]
+        min_coverage = coverage
     print("Covertura minima admitida: ", min_coverage)
     return min_coverage
