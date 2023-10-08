014a4658ca38acc7f3a3e5869e3e2dd0a6486994
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-10-08 11:46:26 -0700

    checkpoint: preparing bash script to run all
    Former-commit-id: a3ca02094e51610f08b36d7c47c0a0b0ff3ed83e

--- src/cluster_kmer_profiles.py
+++ src/cluster_kmer_profiles.py
@@ -5,16 +5,16 @@ import numpy as np
 ## I need to separate clusters of reads by coverage
 ## NOTE: so far only no plants organisms will be tackle.
 
-def cluster_kmer_profiles (kmer_reduction:pd.DataFrame) -> np.ndarray :
+def cluster_kmer_profiles (kmer_reduction_df:pd.DataFrame, n_clusters:int, max_iter:int) -> np.ndarray :
     
     ## Clustering reads using K-means expecting 2 clusters ##
     kmeans = KMeans(
-        n_clusters=2,
-        max_iter=100,
+        n_clusters=n_clusters,
+        max_iter=max_iter,
         init="k-means++",
         random_state=0,
         n_init=1,
         verbose=1,
     )
-    mt_prediction = kmeans.fit_predict(kmer_reduction[["comp1", "comp2"]])
+    mt_prediction = kmeans.fit_predict(kmer_reduction_df[["comp1", "comp2"]])
     return mt_prediction
