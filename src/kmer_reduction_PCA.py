152544bc6a8e95218dd7d56b8a54891ec2b6ed78
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-10-08 11:46:26 -0700

    checkpoint: preparing bash script to run all
    Former-commit-id: a1f23f759bccac0002d92fad76504356cb871730 [formerly a3ca02094e51610f08b36d7c47c0a0b0ff3ed83e]
    Former-commit-id: 014a4658ca38acc7f3a3e5869e3e2dd0a6486994

--- src/kmer_reduction_PCA.py
+++ src/kmer_reduction_PCA.py
@@ -2,16 +2,16 @@ from sklearn.decomposition import PCA
 import pandas as pd
 
 
-def kmer_reduction(kmer_profiles_df:pd.DataFrame, clusters_info:pd.DataFrame) -> pd.DataFrame:
-    pca = PCA(n_components=2)
+def kmer_reduction(kmer_profiles_df:pd.DataFrame, clusters_info:pd.DataFrame, n_comp:int) -> pd.DataFrame:
+    pca = PCA(n_components=n_comp)
     kmer_reduction_df = pd.DataFrame(
         pca.fit_transform(kmer_profiles_df.iloc[:, :-2]), columns=["comp1", "comp2"]
     )
     
     ## Merging the dataframe with ids and other relevant information ##
     kmer_reduction_df["ids"] = kmer_profiles_df["ids"]
-    kmer_reduction_df = kmer_reduction.merge(
+    kmer_reduction_df = kmer_reduction_df.merge(
         clusters_info, how="left", left_on="ids", right_on="id_longest_read"
     )
-    kmer_reduction.drop(columns="id_longest_read", inplace=True)
+    kmer_reduction_df.drop(columns="id_longest_read", inplace=True)
     return kmer_reduction_df
