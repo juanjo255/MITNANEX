8ae9a374c8ca02b6fc70d62ed062b04684516475
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-10-14 12:35:55 -0700

    new branch alternative: here I will try to use an alternative approach where I assembly unitigs and look for mitocondrial genes
    Former-commit-id: 274f5329f884d6b4299f3690df040057702ffa7c

--- src/kmer_reduction_PCA.py
+++ src/kmer_reduction_PCA.py
@@ -15,7 +15,7 @@ def kmer_reduction(kmer_profiles_df:pd.DataFrame, clusters_info:pd.DataFrame, n_
     """
     pca = PCA(n_components=n_comp)
     kmer_reduction_df = pd.DataFrame(
-        pca.fit_transform(kmer_profiles_df.iloc[:, :-2]), columns=["comp1", "comp2"]
+        pca.fit_transform(kmer_profiles_df.iloc[:, :-1]), columns=["comp1", "comp2"]
     )
     
     ## Merging the dataframe with ids and other relevant information ##
