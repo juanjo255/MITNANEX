1ef4e4dd74f9585600216c58fdf8290b7fe11d06
Juan Jose Picon Cossio <jjpiconc@eafit.edu.co>
2023-10-19 19:09:50 -0700

    checkpoint
    Former-commit-id: c20fe59ed410b58a49dd392b1e8573594278b5e5

--- mitnanex_cli.sh
+++ mitnanex_cli.sh
@@ -113,26 +113,34 @@ fi
 
 subsample(){
 ### SEQKIT
+echo " "
 echo $timestamp': Step 1: Sampling with seqkit'
+echo " "
 seqkit seq -g --threads $threads --min-len $min_len --max-len $max_len $input_file | \
     seqkit sample --proportion $proportion --threads $threads -o $wd$prefix"_sample.sorted.fastq"
     
 }
 
 trim_adapters(){
+    echo " "
     echo $timestamp': Step 2: Trimming adapters with porechop'
+    echo " "
     porechop --verbosity 0 -t $threads -o $wd$prefix"_sample.sorted.fastq" -i $wd$prefix"_sample.sorted.fastq"
     
 }
 
 sort_file(){
+    echo " "
     echo $timestamp': Step 3: Sorting file with seqkit'
+    echo " "
     seqkit sort --threads $threads --by-length --reverse -o $wd$prefix"_sample.sorted.fastq" $wd$prefix"_sample.sorted.fastq"
 }
 
 reads_overlap(){
 ### MINIMAP2
+echo " "
 echo $timestamp': Step 4:  Running minimap2'
+echo " "
 minimap2 -x ava-ont -t $threads --dual=yes --split-prefix $prefix \
     $wd$prefix"_sample.sorted.fastq" $wd$prefix"_sample.sorted.fastq" | \
     fpa drop --internalmatch --length-lower $min_len > $wd$prefix"_containments.paf"
@@ -140,14 +148,17 @@ minimap2 -x ava-ont -t $threads --dual=yes --split-prefix $prefix \
 
 mt_reads_filt(){
 ## MITNANEX main
+echo " "
 echo $timestamp': Step 5: Running MITNANEX'
+echo " "
 python3 main.py $wd$prefix"_sample.sorted.fastq" $wd$prefix"_containments.paf" $coverage $map_identity $wd$prefix"_putative_mt_reads.fasta"
 }
 
 first_assembly(){
 ## FIRST DARFT ASSEMBLY 
-
+echo " "
 echo $timestamp': Step 7: Running Miniasm'
+echo " "
 minimap2 -x ava-ont -t $threads --dual=yes --split-prefix $prefix \
     $wd$prefix"_putative_mt_reads.fasta" $wd$prefix"_putative_mt_reads.fasta" | \
     miniasm -f $wd$prefix"_putative_mt_reads.fasta" - > $wd$prefix"_first_draft_asm.gfa"
@@ -157,8 +168,9 @@ minimap2 -x ava-ont -t $threads --dual=yes --split-prefix $prefix \
 statistics(){
 ## STATISTICS OF FIRST DRAFT ASSEMBLY
     echo " "  
-    echo "#### DESCRIPTION OF CONTIGS FOUND #### "
     echo $timestamp': Step 8: Computing statistics with gfastats'
+    echo " "
+    echo "#### DESCRIPTION OF CONTIGS FOUND #### "
     gfastats  --seq-report --discover-paths $wd$prefix"_first_draft_asm.gfa"
     echo " "
 }
@@ -166,7 +178,9 @@ statistics(){
 gfa2fasta(){
 ## SELECT CONTIG AND SUMMON MORE READS
     ## python3 src/select_contig.py $wd$prefix"_flye/" $wd$prefix"_first_draft_mt_assembly.fasta"
+    echo " "
     echo $timestamp': Step 9: Converting gfa to fasta'
+    echo " "
     ### convert form gfa to fasta
     gfastats --discover-paths $wd$prefix"_first_draft_asm.gfa" -o $wd$prefix"_first_draft_asm.fasta" > /dev/null
     
@@ -174,14 +188,20 @@ gfa2fasta(){
 
 collecting_mt_reads(){
 ## USING MINIASM ASSEMBLY COLLECT MORE READS
+    echo " "
+    echo $timestamp': Step 10: Collecting more reads using the draft assembly with minimap2'
+    echo " "
     ### Map reads to the unitig formed by miniasm
     minimap2 -ax map-ont --split-prefix $prefix  $wd$prefix"_first_draft_asm.fasta" $input_file -o $wd$prefix"_align.sam"
     ### Get correctly mapped reads
+    echo " "
+    echo $timestamp" : Reads collected for final assembly"
     samtools view --min-MQ $min_qual -F 4 --bam $wd$prefix"_align.sam" | samtools fastq > $wd$prefix"_collected_reads.fastq"
+    echo " "
 }
 
 final_assembly(){
-    echo $timestamp': Step 10: Running final assembly with Flye'
+    echo $timestamp': Step 11: Running final assembly with Flye'
     flye --scaffold -t $threads --iterations 5 --no-alt-contigs --nano-raw $wd$prefix"_collected_reads.fastq" -o $wd$prefix"_flye/"
 }
 
@@ -210,6 +230,14 @@ start=$SECONDS
 create_wd && subsample && trim_adapters && sort_file && reads_overlap && mt_reads_filt \
 && first_assembly && statistics && gfa2fasta && collecting_mt_reads && final_assembly
 
+echo ""
+echo "### MITNANEX finished ###"
+echo ""
+echo "Final assembly is in" $wd$prefix"_flye""/assembly.fasta"
+echo "Putative mitochondrial reads are in "$wd$prefix"_collected_reads.fastq"
+
 ## END TIMER
 duration=$(( SECONDS - start ))
+echo "" 
 echo "$timestamp -> Elapsed time: $duration secs."
+echo "" 
