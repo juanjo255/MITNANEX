9460db7c412df87f882b0a19568851744475974f
juanjo255 <jjpiconc@eafit.edu.co>
2024-10-15 18:01:05 -0500

    adding mapping

--- mitnanex_reference.sh
+++ mitnanex_reference.sh
@@ -5,42 +5,82 @@ wd="."
 output_folder="mitnanex_results"
 threads=4
 min_qual=0
+minimap2_opts="-ax map-ont"
+min_mapQ=30
 
-
-## Help message
-mitnanex_help() {
+## HELP MESSAGE
+help() {
     echo "
     MITNANEX - MITochondrial NANopore reads EXtractor
 
-    Author:
-    Juan Picon Cossio
 
-    Version: 0.1
-
-    Usage: mitnanex.sh [options] FASTQ
+    Usage: mitnanex.sh --reference genome.fasta --reads reads.fastq [options]
 
     Options:
-        -i        Input file. [required].
-        -t        Threads. [4].
-        -m        Min-len. Filter reads by minimun length. Read seqkit seq documentation. [-1].
-        -M        Max-len. Filter reads by maximun length. Read seqkit seq documentation. [-1].
-        -w        Working directory. Path to create the folder which will contain all mitnanex information. [$wd].
-        -s        Mapping identity. Minimun identity between two reads to be store in the same cluster.[0.6].
-        -q        Min mapping quality (>=). This is for samtools. [0].
-        -f        Flye mode. [--nano-hq]
-        -k        Percentage of reads to keep during filter with filtlong. [80]. 
-        *         Help.
+        -r, --reference    Reference Genome [required].
+        -i, --reads        Input file. [required].
+        -t, --threads      Threads. [$threads].
+        --mm2              Minimap2 options.[$minimap2_opts].
+        --wd               Working Directory to place results. [$wd]
+        -o, --output       Outout directory. [$output_folder].
+        --mapq             Minimun mapping quality. [$min_mapQ].
+        *                  Help.
     "
     exit 1
 }
 
+## PARSE ARGUMENTS
+ARGS=$(getopt -o "hr:i:t:" --long "help,reference:,reads:,mm2:,threads:," -n 'MITNANEX' -- "$@")
+eval set -- "$ARGS"
+
+while true;do
+    case $1 in
+    -r | --reference)
+        ref_genome=$2
+        shift 2
+    ;;
+    -i | --reads)
+        reads=$2
+        shift 2
+    ;;
+    --mm2)
+        minimap2_opts=$2
+        shift 2
+    ;;
+    --mapq)
+        min_mapQ=$2
+        shift 2
+    ;;
+    --help | -h)
+        help 
+    ;;
+    --) 
+        shift 1
+    ;;
+    *)
+        echo "ERROR: Invalid option"
+    ;;
+    esac
+    break
+done
+
+
+if [ ${wd: -1} = / ];
+then 
+    wd=$wd$output_folder
+else
+    wd=$wd"/"$output_folder
+fi
+
+## PREFIX name to use for the resulting files
+if [ -z $prefix ];
+then 
+    prefix=$(basename $reads)
+    prefix=${prefix%%.*}
+fi
 
 map_reads(){
-    # Input
-    ## $1=reference
-    ## $2=query
-    ## $3=output
     
-    minimap2 -ax map-ont --split-prefix "temp_prefix" --secondary=no  $1 $2 | samtools view -b --min-MQ $min_qual -F4 -T $1 -o $3
-
+    minimap2 $minimap2_opts --split-prefix "temp" --secondary=no  $ref_genome $reads | samtools view -b --min-MQ $min_mapQ -F4 -T $ref_genome -o "$wd/$prefix.bam"
 }
+
