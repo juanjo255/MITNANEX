10bda819d2db11218ab8ecc55ac5e4c9fec6aab0
juanjo255 <piconcossio@gmail.com>
2024-10-24 09:25:15 -0500

    sorting this out

--- mitnanex_reference.sh
+++ mitnanex_reference.sh
@@ -258,19 +258,25 @@ map_reads(){
     custom_prints "Retrieve mitochondria and remap reads"
     # Retrieve the mitochondria in the flye assembly which is the one with the highest coverage. 
     contig_ID=$(sort -n -k3 $flye_folder"assembly_info.txt" | tail -n 1 | cut -f 1)
+
+    ## Save mitogenome flye consensus
+    consensus_mitogenome=$(seqkit grep -p $contig_ID "$flye_folder/assembly.fasta")
+
+    ## Retrieve reads which mapped to the consensus_mitogenome 
     samtools view -@ $threads -b -F2048 $flye_folder"/aln_"$prefix".sorted.bam" $contig_ID >  $flye_folder"/aln_"$prefix"_$contig_ID.bam"
-    samtools sort -@ $threads > $flye_folder"/aln_"$prefix"_$contig_ID.sorted.bam"
+    samtools sort -@ $threads $flye_folder"/aln_"$prefix"_$contig_ID.bam" > $flye_folder"/aln_"$prefix"_$contig_ID.sorted.bam"
     samtools fastq -@ $threads $flye_folder"/aln_"$prefix"_$contig_ID.sorted.bam" > $MT_reads
-    #rm $flye_folder"/aln_"$prefix"_$contig_ID.bam"
 
     ## Removing unneeded files
-    #rm $flye_folder"/aln_"$prefix".sorted.bam"
+    rm $flye_folder"/aln_"$prefix"_$contig_ID.bam"
+    rm $flye_folder"/aln_"$prefix".sorted.bam"
 
     ## Final align file for variant calling 
     minimap2 --secondary=no -R '@RG\tID:samplename\tSM:samplename' $minimap2_opts $ref_genome $MT_reads | \
     samtools view -@ $threads -b -F2052 -T $ref_genome | samtools sort -@ $threads > $aln_file
 
     ## PRINT OUTPUT SUMMARY
+    echo "$timestamp [ATTENTION]: Consensus mitogenome is at" $consensus_mitogenome
     echo "$timestamp [ATTENTION]: Mitochondrial reads are at: " $MT_reads
     echo "$timestamp [ATTENTION]: Mitochondrial reads mapped mitochondrial reference $(basename $ref_genome) at: " $aln_file
     num_mapped_reads=$(samtools view -c $aln_file)
