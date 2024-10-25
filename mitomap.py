ff79ab644c982a6e33c42e31a8b3d75099f9ed2f
juanjo255 <piconcossio@gmail.com>
2024-10-25 16:05:56 -0500

    changes in strategy

---
+++ mitomap.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    try:
+        response = requests.post("https://mitomap.org/mitomaster/websrvc.cgi", files={"file": open(args[2]),'fileType': ('', 'sequences'),'output': ('', 'detail')})
+        print(str(response.content, 'utf-8'))
+        file_mitomap = open(args[1], "w")
+        file_mitomap.write(str(response.content, 'utf-8'))
+        file_mitomap.close()
+    except requests.exceptions.HTTPError as err:
+        print("HTTP error: " + err)
+    except:
+        print("Error")  
