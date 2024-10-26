b5c85a77056bfbeb6bfce24553ac0e9a4dbcb04e
juanjo255 <piconcossio@gmail.com>
2024-10-26 10:27:07 -0500

    annotate in mitomap snv list and not fasta sequence

--- mitomap.py
+++ mitomap.py
@@ -5,8 +5,8 @@ import sys
 if __name__ == "__main__":
     args = sys.argv
     try:
-        response = requests.post("https://mitomap.org/mitomaster/websrvc.cgi", files={"file": open(args[2]),'fileType': ('', 'sequences'),'output': ('', 'detail')})
-        print(str(response.content, 'utf-8'))
+        response = requests.post("https://mitomap.org/mitomaster/websrvc.cgi", files={"file": open(args[2]),'fileType': ('', 'snvlist'),'output': ('', 'detail')})
+        #print(str(response.content, 'utf-8'))
         file_mitomap = open(args[1], "w")
         file_mitomap.write(str(response.content, 'utf-8'))
         file_mitomap.close()
