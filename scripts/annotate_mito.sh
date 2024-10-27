10df8d32cf551da17484250727e21ab1a824ff2f
juanjo255 <piconcossio@gmail.com>
2024-10-27 10:52:42 -0500

    I will use liftoff to annotate mitogenome

--- scripts/annotate_mito.sh
+++ scripts/annotate_mito.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 annotate_mito(){
-    
+
+    custom_prints "Lift genes from mitogenome reference to mitogenome of current sample"
+
+    annotation_file="$WD/annotation/$prefix.liftoff.gff"
+
+    liftoff -g "$exec_path/refseqMT/chrMT_NC_012920.gff3" "$consensus_mitogenome" \
+    "$exec_path/refseqMT/chrMT.fna" -cds -o $annotation_file
+
+    echo "$timestamp [ATTENTION]: Mitochondrial consensus annotation file is at" $annotation_file
+
 }
