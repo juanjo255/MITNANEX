9a90d3dfbbf7d66bca7dcf677b97f8e83ec3c78a
juanjo255 <jjpiconc@eafit.edu.co>
2024-10-30 19:16:46 -0500

    adding more plots. AF density

--- scripts/custom_macro.py
+++ scripts/custom_macro.py
@@ -1,7 +1,13 @@
 from vcfstats.macros import continuous, categorical, aggregation
+import numpy as np
 
 @categorical
 def POS(variant):
     """Get the position for each variant as a numpy array."""
-    return variant.POS    
+    return variant.POS
 
+    
+@continuous
+def AF(variant):
+    """Get allele fractions of alternate alleles as a numpy array."""
+    return np.concatenate([variant.format("AF").flatten()])
