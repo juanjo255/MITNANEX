f2c55bb4dca815409d28e45f6a1f14e8575f7321
juanjo255 <jjpiconc@eafit.edu.co>
2024-10-30 19:56:02 -0500

    plot for MBQ

--- scripts/custom_macro.py
+++ scripts/custom_macro.py
@@ -11,3 +11,8 @@ def POS(variant):
 def AF(variant):
     """Get allele fractions of alternate alleles as a numpy array."""
     return np.concatenate([variant.format("AF").flatten()])
+
+@continuous
+def MBQ(variant):
+    """Get median base quality by allele as a numpy array."""
+    return [i for i in variant.INFO["MBQ"]]
