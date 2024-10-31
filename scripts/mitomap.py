3bcf5dfe116e87c3a162d3ef7f0eb484a23a2ccb
juanjo255 <piconcossio@gmail.com>
2024-10-31 10:07:06 -0500

    updating requirements

--- scripts/mitomap.py
+++ scripts/mitomap.py
@@ -4,10 +4,12 @@ import sys
 
 if __name__ == "__main__":
     args = sys.argv
+    # args [1] input file
+    # args [2] output file
     try:
-        response = requests.post("https://mitomap.org/mitomaster/websrvc.cgi", files={"file": open(args[2]),'fileType': ('', 'snvlist'),'output': ('', 'detail')})
+        response = requests.post("https://mitomap.org/mitomaster/websrvc.cgi", files={"file": open(args[1]),'fileType': ('', 'snvlist'),'output': ('', 'detail')})
         #print(str(response.content, 'utf-8'))
-        file_mitomap = open(args[1], "w")
+        file_mitomap = open(args[2], "w")
         file_mitomap.write(str(response.content, 'utf-8'))
         file_mitomap.close()
     except requests.exceptions.HTTPError as err:
