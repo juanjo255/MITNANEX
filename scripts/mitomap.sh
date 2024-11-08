5070a0a6f688084db469374f83374d167322bd32
juanjo255 <piconcossio@gmail.com>
2024-11-08 10:03:21 -0500

    I had to transformed the vcf to a tsv before using mitomaster

--- scripts/mitomap.sh
+++ scripts/mitomap.sh
@@ -7,9 +7,9 @@ mitomap() {
     mitomap_out="$WD/variant_annot.mitomap.txt"
 
     # Convert VCF to TSV-like file
-    #bcftools query -i 'FILTER="PASS"' -f '%CHROM\t%POS\t%REF\t%ALT' all_20240815_human.NC_012920.1.gatk.filt.anno.vcf > prueba.vcf
-
-    $exec_path/scripts/mitomap.py $vcf_file $mitomap_out
+    bcftools query -i 'FILTER="PASS"' -f '%CHROM\t%POS\t%REF\t%ALT' $vcf_file > "$vcf_file.tsv"
+    trans_vcf="$vcf_file.tsv"
+    $exec_path/scripts/mitomap.py $trans_vcf $mitomap_out
 
     echo "$timestamp [ATTENTION]: Variant annotations are at" $mitomap_out
 
